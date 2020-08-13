23791d624c007c382e3a6f84e05b46971eb561bd
ricardocvel <ricardocampos_pr@live.com>
2020-08-13 17:15:11 -0300

    desenvolvimento login

--- expts_app/src/pages/Login.js
+++ expts_app/src/pages/Login.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { 
+  Text, View, StyleSheet, StatusBar, Image, TextInput
+} from 'react-native';
+
+//media
+import logo from '../assets/logo.png';
 
 const Login = () =>  {
   return (
-    <View>
+    <View style={styles.container}>
+      <StatusBar style={styles.headbar} barStyle = "light-content" />
+      <Image source={logo} />
+      <TextInput 
+        placeholder="Login"
+        style={styles.input}
+        />
       <Text>Expertise Teste Teste</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  headbar:{
+    backgroundColor: "#003399",
+  },
+
   container: {
-    
+    flex: 1,
+    backgroundColor: "#F2F5F7",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  imput: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: "#FFFF",
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+
+  foot: {
+
   }
+
 });
 
 export default Login();
+
+
 
