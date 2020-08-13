7bca9a5954067e835e281def07d3db02f3fef284
ricardocvel <ricardocampos_pr@live.com>
2020-08-13 15:58:41 -0300

    estruturação do app

---
+++ expts_app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Paginas
+import Login from './pages/Login';
+
+const App = () =>  {
+  return (
+    Login
+  );
+}
+
+export default App;
---
+++ expts_app/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Login = () =>  {
+  return (
+    <View>
+      <Text>Expertise Teste Teste</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+});
+
+export default Login();
+
