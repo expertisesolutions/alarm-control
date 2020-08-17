d561651b507c57bb98f1aa2a1827cfe08d62f056
ricardocvel <ricardocampos_pr@live.com>
2020-08-17 15:20:13 -0300

    TESTE - commit de teste -   criando tela para teste de conexão com home assistant [update].

--- expts_app/src/pages/Test.js
+++ expts_app/src/pages/Test.js
@@ -1,105 +1,91 @@
 import React from 'react';
+
 import { 
   Text, View, StyleSheet, StatusBar, Image, TextInput,
   TouchableOpacity, KeyboardAvoidingView, Platform
 } from 'react-native';
-
-//media
-import logo from '../assets/logo.png';
-import server from '../assets/server.png';
-
-export default function Test({navigation}) {
+  
+export default function Login({navigation}) {
   function handloLogin(){
-   navigation.navigate('Main');
+    navigation.navigate('Main');
   }
-
-  return (
-  <KeyboardAvoidingView 
-    behavior="padding"
-    enabled={Platform.OS === 'ios'}
-    style={styles.containerbackground}
-    >
-    <StatusBar  barStyle="light-content" backgroundColor="#003399" />
-    <View style={styles.container}>
-      <Image source={logo} />
-    </View>
-    <View style={styles.container}>
-    <Image source={server} />
-     <TouchableOpacity onPress={handloLogin} style={styles.button}>
-        <Text style={styles.buttonText}></Text>
-      </TouchableOpacity>
-    </View >
-    <View style={styles.foot}>
-    </View>
-  </KeyboardAvoidingView>
-  );
-};
-
-const styles = StyleSheet.create({
-  headbar:{
-    backgroundColor: "#003399",
-  },
-
-  containerbackground: {
-    flex: 1,
-    backgroundColor: "#F2F5F7",
-    height: 'auto',
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: "#F2F5F7",
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 30,
-  },
-
-  input: {
-    height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: "#FFFF",
-    borderWidth: 1,
-    borderColor: '#F2F5F7',
-    borderRadius: 4,
-    marginTop: 20,
-    marginHorizontal: 10,
-    paddingHorizontal: 15,
-  },
-
-  button: {
-    height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: "#6FCF97",
-    borderRadius: 4,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-
-  buttonOff: {
-    height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: "#E0E0E0",
-    borderRadius: 4,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-
-  buttonText: {
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-
-  foot: {
-    flex: 1,
-    backgroundColor: "#003399",
-    position: 'relative',
-    height: 20,
-    left: 0,
-    top: 175,
-  },
-});
+  
+    return (
+      <KeyboardAvoidingView 
+        behavior="padding"
+        enabled={Platform.OS === 'ios'}
+        style={styles.containerbackground}
+        >
+        <StatusBar  barStyle="light-content" backgroundColor="#003399" />
+        <View style={styles.container}>
+          <TextInput 
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Login"
+            placeholderTextColor="#999"
+            style={styles.input}
+          />
+          <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Pass**"
+            placeholderTextColor="#999"
+            style={styles.input}
+          />
+          <TouchableOpacity onPress={handloLogin} style={styles.button}>
+            <Text style={styles.buttonText}>Test</Text>
+          </TouchableOpacity>
+        </View >
+      </KeyboardAvoidingView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    headbar:{
+      backgroundColor: "#003399",
+    },
+  
+    containerbackground: {
+      flex: 1,
+      backgroundColor: "#F2F5F7",
+      height: 'auto',
+    },
+  
+    container: {
+      flex: 1,
+      backgroundColor: "#F2F5F7",
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 30,
+    },
+  
+    input: {
+      height: 46,
+      alignSelf: 'stretch',
+      backgroundColor: "#FFFF",
+      borderWidth: 1,
+      borderColor: '#F2F5F7',
+      borderRadius: 4,
+      marginTop: 20,
+      marginHorizontal: 10,
+      paddingHorizontal: 15,
+    },
+  
+    button: {
+      height: 46,
+      alignSelf: 'stretch',
+      backgroundColor: "#003399",
+      borderRadius: 4,
+      marginTop: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginHorizontal: 10,
+    },
+  
+    buttonText: {
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  
+  });
