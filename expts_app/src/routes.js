afa1c26e7a20229702f706b73dbba97a3ed3aeb1
ricardocvel <ricardocampos_pr@live.com>
2020-08-20 14:07:02 -0300

    adicionando parametros para o teste, ( link direto conform documentação)

--- expts_app/src/routes.js
+++ expts_app/src/routes.js
@@ -5,9 +5,11 @@ import Register from './pages/register';
 import Login from './pages/Login'
 import Main from './pages/Main'
 import Test from './pages/Test'
+import Testoauth from './pages/Linkdireto'
 
 const Routes = createAppContainer(
   createSwitchNavigator({
+    Testoauth,
     Login,
     Main,
     Register,
