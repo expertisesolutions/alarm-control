e018c8da3c498f1083fcef0e7208a13e5df27817
ricardocvel <ricardocampos_pr@live.com>
2020-08-28 16:05:02 -0300

    adição de paginas

---
+++ expts_app/src/pages/Addserver.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+export default function Addserver({navigation}) {
+  function handloLogin(){
+   navigation.navigate('Main');
+  }
+
+return (
+  <Text>AddServer</Text>
+ );
+};
+
+
+const styles = StyleSheet.create({
+
+});
---
+++ expts_app/src/pages/Armaralarm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+export default function Ativaalarm({navigation}) {
+  function handloLogin(){
+   navigation.navigate('Main');
+  }
+
+return (
+  <Text>Ativar alarme </Text>
+ );
+};
+
+
+const styles = StyleSheet.create({
+
+});
