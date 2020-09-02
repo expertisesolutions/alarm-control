f9dd1d7f3edd83bb667deb340c7e49cb43cecb10
ricardocvel <ricardocampos_pr@live.com>
2020-09-02 16:11:47 -0300

    estrutura da pagina + estilização da estrutura

--- expts_app/src/pages/Addserver.js
+++ expts_app/src/pages/Addserver.js
@@ -4,6 +4,11 @@ import {
 
 } from 'react-native';
 
+//media
+import left from '../assets/left.png' ;
+import logo from '../assets/logo.png';
+
+
 export default function Addserver({navigation}) {
   function handloLogin(){
    navigation.navigate('Main');
@@ -15,11 +20,19 @@ export default function Addserver({navigation}) {
         enabled={Platform.OS === 'ios'}
         style={styles.containerbackground}
     >
+      {/* Status bar */}
       <StatusBar  barStyle="light-content" backgroundColor="#7C7B7B" />
-      <View style={styles.head}>
-       <Image source={logo} />
-      </View>
 
+      {/* HEAD */}
+      <View style={styles.container_head}>
+       <Image style={styles.img_left} source={left} />
+       <Text style={styles.text_head}> Adicionar Server</Text>
+      </View>
+      
+      {/* BODY */}
+      <View style={styles.container_logo}>
+       <Image style={styles.img_logo} source={logo} />
+      </View>
 
 
     </KeyboardAvoidingView>
@@ -30,15 +43,48 @@ export default function Addserver({navigation}) {
 const styles = StyleSheet.create({
   containerbackground: {
     flex: 1,
-    backgroundColor: "#F2F5F7",
-    height: 'auto',
+    //backgroundColor: "#F2F5F7",
+    paddingHorizontal: 0,
+    //paddingTop: Constant
   },
 
-  head: {
-    flex: 1,
+  container_head: {
+    flexDirection: 'row',
+   //justifyContent: 'center' ,
     backgroundColor: "#003399",
-    height: 2,
+    height: 50,
+    alignItems: 'center',
+    padding: 10,
   },
 
+  container_logo: {
+    flex: 1,
+    backgroundColor: "#F2F5F7",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  img_left: {
+    height: 20,
+    width: 20,
+    marginTop: 5,
+  },
+
+  img_logo: {
+    height: 37,
+    width: 37,
+  },
+
+  text_head:{
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  }
 
 });
+
+//justifyContent: 'center',
+//    alignItems: 'center',
+//    padding: 30,
