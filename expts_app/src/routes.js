9d80a9adabe1286049717ed1d238a6320a3eb98d
ricardocvel <ricardocampos_pr@live.com>
2020-09-14 16:49:03 -0300

    Personalizando as rotas

--- expts_app/src/routes.js
+++ expts_app/src/routes.js
@@ -1,24 +1,56 @@
 import {createAppContainer} from 'react-navigation';
 import {createSwitchNavigator} from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
 import Register from './pages/register';
 import Login from './pages/Login'
 import Main from './pages/Main'
 import Test from './pages/Test'
-import Addserver  from './pages/Addserver'
+import Addserver from './pages/Addserver'
 import Ativaalarm from './pages/Armaralarm'
 import Mainnext from './pages/Mainnext'
 
-const Routes = createAppContainer(
-  createSwitchNavigator({
-    Login,
-    Main,
-    Register,
-    Test,
-    Addserver,
-    Ativaalarm,
-    Mainnext,
-  })
-);
 
-export default Routes;
+const Auth = createSwitchNavigator({
+  Auth: {
+  screen: Login
+},
+
+});
+
+const Menudrawer = createDrawerNavigator({
+
+  Main: {
+    screen: Main,
+  },
+  Register: {
+    screen: Register,
+  },
+  Test: {
+    screen: Test,
+  },
+  Addserver: {
+    screen: Addserver,
+  },
+  Ativaalarm: {
+    screen: Ativaalarm,
+  },
+  Mainnext:{
+    screen: Mainnext,
+  },
+});
+
+const Routes = createSwitchNavigator({
+  Auth: {
+    screen: Login
+  },
+  Test: {
+    screen: Menudrawer,
+  },
+});
+
+export default createAppContainer(Routes);
+
+// Rotas: {
+// screen: Routes,
+// },
