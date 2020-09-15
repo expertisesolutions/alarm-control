a387fa5591dc637b42ec6870f2ff796d09449cc9
ricardocvel <ricardocampos_pr@live.com>
2020-09-15 11:14:14 -0300

    adicionado estilização do Menu

--- expts_app/src/pages/CustonDrwer.js
+++ expts_app/src/pages/CustonDrwer.js
@@ -18,13 +18,35 @@ export default function CustonDrawer({...props}){
     <View style={styles.containerbackground}>
       <View style={styles.viewHead}>
         <Image source={Menu} style={styles.iconHead}/>
-        <Text style={styles.textoHead}> Titulo</Text>
+        <Text style={styles.textoHead}> Menu</Text>
       </View>
       <View style={styles.viewItens}>
+
         <View style={styles.viewItem}>
           <Image source={Menu} style={styles.iconItem}/>
           <Text style={styles.textoItem}> Texto Item</Text>
         </View>
+
+        <View style={styles.viewItem}>
+          <Image source={Menu} style={styles.iconItem}/>
+          <Text style={styles.textoItem}> Texto Item</Text>
+        </View>
+
+        <View style={styles.viewItem}>
+          <Image source={Menu} style={styles.iconItem}/>
+          <Text style={styles.textoItem}> Texto Item</Text>
+        </View>
+
+        <View style={styles.viewItem}>
+          <Image source={Menu} style={styles.iconItem}/>
+          <Text style={styles.textoItem}> Texto Item</Text>
+        </View>
+
+        <View style={styles.viewItem}>
+          <Image source={Menu} style={styles.iconItem}/>
+          <Text style={styles.textoItem}> Texto Item</Text>
+        </View>
+
       </View>
       
     </View>
@@ -35,26 +57,49 @@ const styles = StyleSheet.create({
 
   containerbackground: {
     flex: 1,
-    backgroundColor: "#C4C4C4",
+    backgroundColor: "#8A8A8A",
     justifyContent:'space-between',
   },
 
+  viewHead:{
+    flexDirection: 'row',
+    alignItems:'center',
+    height:'10%',
+    padding: 15,
+  },
+
   viewItens:{
+    height:'90%',
   },
 
   viewItem:{
+    flexDirection: 'row',
+    alignItems:'center',
+    padding: 18,
   },
 
   iconHead:{
+    height: 40,
+    width: 40,
   },
 
   iconItem:{
+    height: 30,
+    width: 30,
   },
 
   textoHead:{
+    marginLeft: 15,
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight:'bold',
   },
 
   textoItem:{
+    marginLeft: 20,
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'normal',
   },
 });
 
