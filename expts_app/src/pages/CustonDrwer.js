fcb21df1c55069c72e45a3a16abd03125036c706
ricardocvel <ricardocampos_pr@live.com>
2020-09-15 10:44:15 -0300

    adicionado styles faltantes

--- expts_app/src/pages/CustonDrwer.js
+++ expts_app/src/pages/CustonDrwer.js
@@ -17,13 +17,13 @@ export default function CustonDrawer({...props}){
   return(
     <View style={styles.containerbackground}>
       <View style={styles.viewHead}>
-        <Image source={Menu}/>
-        <Text> Titulo</Text>
+        <Image source={Menu} style={styles.iconHead}/>
+        <Text style={styles.textoHead}> Titulo</Text>
       </View>
       <View style={styles.viewItens}>
         <View style={styles.viewItem}>
-          <Image source={Menu}/>
-          <Text> Texto Item</Text>
+          <Image source={Menu} style={styles.iconItem}/>
+          <Text style={styles.textoItem}> Texto Item</Text>
         </View>
       </View>
       
