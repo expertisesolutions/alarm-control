d89f502c287ee4ae2640e93cdcd18fd5f918adb9
ricardocvel <ricardocampos_pr@live.com>
2020-10-29 15:33:52 -0300

    Configuração Hass

--- expts_app/src/pages/components/Conexao.js
+++ expts_app/src/pages/components/Conexao.js
@@ -1,4 +1,6 @@
 
+
+/*  
 import {
   getAuth,
   createConnection,
@@ -30,4 +32,25 @@ async function connect() {
  
 connect();
 
+*/
 
+export default configHass({
+  protocol: 'ws',
+  host: '192.168.0.29/',
+  port: 8123,
+  path: '/api/websocket',
+ 
+  // Must be set if HA expects authentication:
+  token: null,
+ 
+  // Used to serialize outgoing messages: 
+  messageSerializer: outgoingMessage => JSON.stringify(outgoingMessage),
+ 
+  // Used to parse incoming messages. Receives the entire Websocket message object:
+  messageParser: incomingMessage => JSON.parse(incomingMessage.data),
+ 
+  // Should return a WebSocket instance
+  ws (opts) {
+    return new WebSocket(`${opts.protocol}://${opts.host}:${opts.port}${opts.path}`)
+  }
+})
