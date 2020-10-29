23b7e3353e27ae841cfa415c408cf2e476c69fb5
ricardocvel <ricardocampos_pr@live.com>
2020-10-29 15:11:57 -0300

    adicionado  conexao.js como service

---
+++ expts_app/src/pages/components/Conexao.js
@@ -0,0 +1,33 @@
+
+import {
+  getAuth,
+  createConnection,
+  subscribeEntities,
+  ERR_HASS_HOST_REQUIRED,
+} from "home-assistant-js-websocket";
+ 
+async function connect() {
+  let auth;
+  try {
+    // Try to pick up authentication after user logs in
+    auth = await getAuth();
+  } catch (err) {
+    if (err === ERR_HASS_HOST_REQUIRED) {
+      const hassUrl = prompt(
+        "What host to connect to?",
+        "http://localhost:8123"
+      );
+      // Redirect user to log in on their instance
+      auth = await getAuth({ hassUrl });
+    } else {
+      alert(`Unknown error: ${err}`);
+      return;
+    }
+  }
+  const connection = await createConnection({ auth });
+  subscribeEntities(connection, (ent) => console.log(ent));
+}
+ 
+connect();
+
+
